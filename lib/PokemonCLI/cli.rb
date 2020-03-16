fc808f59a8305689647b557c486145409d279949
Andrew Smith <848smith@gmail.com>
2020-03-16 03:49:32 +0000

    Added more functionality to the CLI. Made a way to quit out of it.

--- lib/PokemonCLI/cli.rb
+++ lib/PokemonCLI/cli.rb
@@ -2,19 +2,30 @@ class PokemonCLI::CLI
   def start
     puts "================================================================"
     puts "==========================Hello there!=========================="
-    puts "======Here you can find out information about any pokemon!======"
-    puts "======To find out more info please type the pokemon's name:====="
+    puts "===Here you can find out information about any pokemon!      ==="
+    puts "===At any point in this you can type quit to exit the session==="
+    puts "===To find out more info please type the pokemon's name:     ==="
     input = gets.strip.downcase
-    @data = PokemonCLI::API.get_pokemon(input)
-    display
+    
+    if input == "quit"
+      quit
+    else
+      @data = PokemonCLI::API.get_pokemon(input)
+      display
+    end
   end
   
   def display
-    puts "===============You have chosen the pokemon #{@data.name}!==============="
-    puts "======Select a number below to find out more about #{@data.name}:======="
+    puts "===You have chosen the pokemon #{@data.name}!                      ==="
+    puts "===Select a number below to find out more about #{@data.name}:     ==="
+    puts "===1. All games #{@data.name} is listed in.                        ==="
+    puts "===2. Stats for #{@data.name}.                                     ==="
+    puts "===3. What type and where in the pokedex #{@data.name} is located. ==="
+    puts "===4. Basic information for #{@data.name}.                         ==="
   end
   
   def quit
-    
+    puts "============================Goodbye!============================"
+    puts "================================================================"
   end
 end
