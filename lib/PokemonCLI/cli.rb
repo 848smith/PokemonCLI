17186a19df205719bd0f730cd61cbfbed29026ac
Andrew Smith <848smith@gmail.com>
2020-03-16 03:14:40 +0000

    Started making the CLI that the user will see. Need to add a second level on it and a way to quit.

--- lib/PokemonCLI/cli.rb
+++ lib/PokemonCLI/cli.rb
@@ -1,8 +1,20 @@
 class PokemonCLI::CLI
   def start
-    puts "Gotta catch them all"
-    input = "charmander"
+    puts "================================================================"
+    puts "==========================Hello there!=========================="
+    puts "======Here you can find out information about any pokemon!======"
+    puts "======To find out more info please type the pokemon's name:====="
+    input = gets.strip.downcase
     @data = PokemonCLI::API.get_pokemon(input)
-    binding.pry
+    display
+  end
+  
+  def display
+    puts "===============You have chosen the pokemon #{@data.name}!==============="
+    puts "======Select a number below to find out more about #{@data.name}:======="
+  end
+  
+  def quit
+    
   end
 end
